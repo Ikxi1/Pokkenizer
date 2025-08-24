0489073a9c446fe7ac1b370bb68ecbc2c3c2234a
Ikxi <ikxi@ikxi.art>
2025-08-24 13:07:21 +0200

    trying to tokenize the tokens with the persistent delimeters

--- asm_solution/source/main.asm
+++ asm_solution/source/main.asm
@@ -6,10 +6,12 @@ extern ascii_to_bytes
 
 main:
 open_file:
-    cmp rdi, 3
+    cmp rdi, 4
     jne argc_fail
     mov rax, qword [rsi + 16]
-    mov [delimiter_pointer], qword rax
+    mov [delimiter_pointer1], qword rax
+    mov rax, qword [rsi + 24]
+    mov [delimiter_pointer2], qword rax
     mov rax, 2
     mov rdi, qword [rsi + 8]
     ; mov rdi, [rsp + 16]
@@ -23,7 +25,6 @@ open_file:
     test rax, rax
     js exit_bad_file
     mov qword [file_pointer], rax
-    ; call get_delimiters1
 get_file_size:
     ; lseek syscall == fseek in c
     mov rax, 8
@@ -33,13 +34,15 @@ get_file_size:
     syscall
     test rax, rax
     js exit_size_fail
-    mov qword [file_size], rax
-    mov r11, rax
+    mov rsi, rax
+    clc
+    shl rax, 1
+    jc file_too_big
+    mov [file_size], qword rsi
     call reset_file_ptr
-allocate_file_buffer1:
+allocate_file_buffer:
     mov rax, 9
     xor rdi, rdi
-    mov rsi, qword [file_size]
     mov rdx, 3
     mov r10, 0x22
     xor r9, r9
@@ -47,62 +50,29 @@ allocate_file_buffer1:
     syscall
     test rax, rax
     js something_else
-    mov qword [file_buffer_pointer1], rax
-allocate_file_buffer2:
-    mov rax, 9
-    xor rdi, rdi
-    mov rsi, qword [file_size]
-    mov rdx, 3
-    mov r10, 0x22
-    xor r9, r9
-    xor r8, r8
-    syscall
-    test rax, rax
-    js something_else
-    mov qword [file_buffer_pointer2], rax
-copy_file1:
+    mov qword [file_buffer_pointer], rax
+copy_file:
     xor rax, rax
     mov rdi, qword [file_pointer]
-    mov rsi, qword [file_buffer_pointer1]
-    mov rdx, qword [file_size]
+    mov rdx, rsi
+    mov rsi, qword [file_buffer_pointer]
     syscall
     test rax, rax
     js exit_read_fail
     call reset_file_ptr
-copy_file2:
-    xor rax, rax
-    mov rsi, qword [file_buffer_pointer2]
-    mov rdx, qword [file_size]
-    syscall
-    test rax, rax
-    js exit_read_fail
 close_file:
     mov rax, 3
     syscall
     test rax, rax
     js something_else
-count_pokkes:
-    mov rdi, qword [file_buffer_pointer1]
-    mov rsi, [delimiter_pointer]
-    mov rdx, save_pointer
-    call strtok_r
-    mov qword [pokke_count], 1
-count_pokkes_loop:
-    mov rdi, 0
-    mov rsi, [delimiter_pointer]
-    mov rdx, save_pointer
-    call strtok_r
-    test rax, rax
-    js something_else
-    cmp rax, 0
-    je allocate_pokke_buffer
-    inc qword [pokke_count]
-    jmp count_pokkes_loop
 allocate_pokke_buffer:
+    ; multiply filesize x2 for worst case scenario
+    clc
+    shl rdx, 1
+    jc file_too_big
+    mov rsi, rdx
     mov rax, 9
     xor rdi, rdi
-    mov rsi, qword [file_size]
-    add rsi, qword [pokke_count]
     mov rdx, 3
     mov r10, 0x22
     xor r9, r9
@@ -113,15 +83,21 @@ allocate_pokke_buffer:
     mov [pokke_buffer_pointer], qword rax
 
 pokkenize:
-    mov rdi, qword [file_buffer_pointer2]
-    mov rsi, [delimiter_pointer]
+    mov rdi, qword [file_buffer_pointer]
+    mov rsi, [delimiter_pointer1]
     mov rdx, save_pointer
     call strtok_r
+    mov r10, [save_pointer]
+    ; tokenize the token
+pokkenize_pokkenize:
+    mov rsi, [delimiter_pointer2]
+    mov rdx, save_pointer
+    call strtok_r
+    mov r11, [save_pointer]
 write_pokke_buffer:
-    mov rdi, rax
     call get_string_length
     mov [string_position], qword rcx
-    mov r8, [pokke_buffer_pointer]
+    mov r8, qword [pokke_buffer_pointer]
     mov r9, r8
     add r9, rcx
 write_pokke_buffer_loop:
@@ -131,15 +107,28 @@ write_pokke_buffer_loop:
     inc r8
     cmp r8, r9
     jl write_pokke_buffer_loop
+    mov rdi, qword [save_pointer]
+    xor rsi, rsi
+    movzx rdi, byte [rdi]
+    cmp rsi, rdi
+    jne pokkenize_pokkenize_loop
+    inc qword [save_pointer]
+
 pokkenize_loop:
     mov rdi, 0
-    mov rsi, [delimiter_pointer]
+    mov rsi, [delimiter_pointer1]
     mov rdx, save_pointer
     call strtok_r
     test rax, rax
     js something_else
     cmp rax, 0
-    je what
+    je write_tokens
+    mov r10, [save_pointer]
+pokkenize_pokkenize2:
+    mov rsi, [delimiter_pointer2]
+    mov rdx, save_pointer
+    call strtok_r
+
 write_pokke_buffer2:
     call get_string_length
     mov r8, qword [pokke_buffer_pointer]
@@ -154,11 +143,31 @@ write_pokke_buffer_loop2:
     cmp r8, r9
     jl write_pokke_buffer_loop2
     add [string_position], qword rcx
+    mov rdi, qword [save_pointer]
+    xor rsi, rsi
+    movzx rdi, byte [rdi]
+    cmp rsi, rdi
+    jne pokkenize_pokkenize_loop2
+    inc qword [save_pointer]
     jmp pokkenize_loop
-what:
+
+pokkenize_pokkenize_loop:
+    mov rdi, 0
+    mov rsi, [delimiter_pointer2]
+    mov rdx, save_pointer
+    call strtok_r
+    jmp write_pokke_buffer
+
+pokkenize_pokkenize_loop2:
+    mov rdi, 0
+    mov rsi, [delimiter_pointer2]
+    mov rdx, save_pointer
+    call strtok_r
+    jmp write_pokke_buffer2
+
+write_tokens:
     mov rsi, qword [pokke_buffer_pointer]
     mov rdx, qword [string_position]
     call write
-
 
     jmp exit
