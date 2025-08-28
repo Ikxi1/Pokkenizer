9201b6689a8735ca0674488c5d7db2c782910f4e
ikxi <ikxialexander1@gmail.com>
2025-08-28 18:16:11 +0200

    tokenizing tokens works now in C, persistent tokens doesn't work yet, changed how memory is allocated in C, just double the filesize instead of counting tokens, as it's not as easy with double tokenizations

--- c_solution/source/main.c
+++ c_solution/source/main.c
@@ -2,62 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
-int get_token_amount(const char *file_buffer, const char *delimiters, const int size) {
-	char *internal_file_buffer = malloc(size + 1);;
-	memcpy(internal_file_buffer, file_buffer, size + 1);
-	char *save_prt;
-	char *token = strtok_r(internal_file_buffer, delimiters, &save_prt);
-	int token_count = 1;
+// int get_token_amount (const char *file_buffer, const char *delimiters, const int size) {
+// 	char *internal_file_buffer = malloc(size + 1);;
+// 	memcpy(internal_file_buffer, file_buffer, size + 1);
+// 	char *save_prt;
+// 	char *token = strtok_r(internal_file_buffer, delimiters, &save_prt);
+// 	int token_count = 1;
+// 	while (1) {
+// 		// printf("%s\n", token);
+// 		token = strtok_r(NULL, delimiters, &save_prt);
+// 		if (token == NULL || token == "\0") break;
+// 		token_count++;
+// 	}
+// 	free(internal_file_buffer);
+// 	return token_count;
+// }
+
+// Get the sub-tokens of a token
+void get_token_tokens (char *token, const char *persistent_delimiters, char *token_buffer, int *string_position) {
+	char *save_ptr;
+	char *token2 = strtok_r(token, persistent_delimiters, &save_ptr);
+	if (token2 == NULL || token2 == "\0") return;
+	int token_length = (int)strlen(token2) + 1;
+	memcpy(token_buffer + *string_position, token2, token_length);
+	*string_position += token_length;
+
 	while (1) {
-		// printf("%s\n", token);
-		token = strtok_r(NULL, delimiters, &save_prt);
-		if (token == NULL || token == "\0") break;
-		token_count++;
+		token2 = strtok_r(NULL, persistent_delimiters, &save_ptr);
+		if (token2 == NULL || token2 == "\0") return;
+
+		token_length = (int)strlen(token2) + 1;
+		memcpy(token_buffer + *string_position, token2, token_length);
+		*string_position += token_length;
 	}
-	free(internal_file_buffer);
-	return token_count;
 }
 
-void get_tokens(char *file_buffer, const char *delimiters, char *token_buffer) {
+// Get the tokens
+void get_tokens (char *file_buffer, const char *delimiters, const char *persistent_delimiters, char *token_buffer) {
 	char *save_prt;
+	int string_position = 0;
 	char *token = strtok_r(file_buffer, delimiters, &save_prt);
-	int string_position = (int)strlen(token) +1;
-	for (int i = 0; i < string_position; i++) {
-		token_buffer[i] = token[i];
-	}
+
+	get_token_tokens(token, persistent_delimiters, token_buffer, &string_position);
 
 	while (1) {
-		// printf(token);
 		token = strtok_r(NULL, delimiters, &save_prt);
-		if (token == NULL || token == "\0") break;
-
-		int token_length = (int)strlen(token) + 1;
-		for (int i = 0; i < token_length; i++) {
-			token_buffer[i + string_position] = token[i];
-		}
-		string_position += token_length;
+		if (token == NULL || token == "\0") return;
+		get_token_tokens(token, persistent_delimiters, token_buffer, &string_position);
 	}
 }
 
-int main(const int argc, const char * argv[]) {
-	if (argc < 2) return 1;
+int main (const int argc, const char *argv[]) {
+	if (argc < 4) return 1;
 	FILE *fptr = fopen(argv[1], "r");
 	if (fptr == NULL) return 2;
 	fseek(fptr, 0, SEEK_END);
 	const int size = (int)ftell(fptr);
+	if (size == 0) return 3;
 	fseek(fptr, 0, SEEK_SET);
 	char *file_buffer = malloc(size + 1);
 	fread(file_buffer, size, 1, fptr);
 	fclose(fptr);
 
-	const char *delimiters = " \n\t\0";
-	const int token_count = get_token_amount(file_buffer, delimiters, size);
-	char *token_buffer = malloc(size + 1 + token_count);
+	const char *delimiters = " \n\t";
+	const char *persistent_delimiters = "+-*/\\.,;:()[]{}\"\'=#<>&";
+	// const int token_count = get_token_amount(file_buffer, delimiters, size);
+	char *token_buffer = calloc(1, size*2 + 1);
 
-	get_tokens(file_buffer, delimiters, token_buffer);
-	printf(file_buffer);
+	get_tokens(file_buffer, delimiters, persistent_delimiters, token_buffer);
+	// printf(file_buffer);
+	// printf(token_buffer);
+	write(STDOUT_FILENO, token_buffer, size*2 + 1);
 
 	free(token_buffer);
 	free(file_buffer);
